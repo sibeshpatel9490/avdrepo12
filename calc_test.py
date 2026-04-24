bba0bfe304cb5a1eda074cdb36dff979d3ede910
sibesh patel <sibeshpatel.9490@gmail.com>
2026-04-24 20:29:58 +0530

    github actions - test7

--- calc_test.py
+++ calc_test.py
@@ -7,8 +7,8 @@ def test_add():
 def test_sub():
     assert sub(5, 3) == 2
 
-def test_mul():
-    assert mul(2, 3) == 6
+# def test_mul():
+#     assert mul(2, 3) == 6
 
-def test_div():
-    assert div(10, 2) == 5
+# def test_div():
+#     assert div(10, 2) == 5
